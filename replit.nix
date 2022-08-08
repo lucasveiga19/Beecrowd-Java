5bddfa9d8a175ba9bb139afbd08f7482ce171e16
Lucas Uchôa da Veiga <58401882+lucasveiga19@users.noreply.github.com>
2022-08-08 20:46:17 +0000

    Problem-1001

---
+++ replit.nix
@@ -0,0 +1,9 @@
+{ pkgs }: {
+    deps = [
+        pkgs.plan9port
+        pkgs.graalvm17-ce
+        pkgs.maven
+        pkgs.replitPackages.jdt-language-server
+        pkgs.replitPackages.java-debug
+    ];
+}
