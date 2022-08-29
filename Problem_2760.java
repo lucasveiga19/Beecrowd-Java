eeea9c63073aac80477b86c4a8f886c527d41381
Lucas Uchôa da Veiga <58401882+lucasveiga19@users.noreply.github.com>
2022-08-29 22:14:33 +0000

    Activity 2760

---
+++ Problem_2760.java
@@ -0,0 +1,21 @@
+import java.io.IOException;
+import java.util.Scanner;
+
+public class Problem_2760 {
+ 
+    public static void main(String[] args) throws IOException {
+      /*Scanner entrada = new Scanner(System.in);
+
+      String[] a = entrada.nextLine();
+      String[] b = entrada.nextLine();
+      String[] c = entrada.nextLine();
+
+      System.out.println(a+b+c);
+      System.out.println(b+c+a);
+      System.out.println(String.format("%.10c",a+"%.10c",b+"%.10c",c));
+      //System.out.println(a.length(10)+b.length(10)+c.length(10)));
+      
+      entrada.close();*/
+    }
+ 
+}
